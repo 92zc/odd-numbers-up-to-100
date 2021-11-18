a828763642d30ea22067eef489039a3711cac785
92zc <94534546+92zc@users.noreply.github.com>
2021-11-18 11:32:14 +0800

    Add files via upload

---
+++ 作业.cpp
@@ -0,0 +1,27 @@
+//打出100以内的奇数
+
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	while(i<=100)
+	{
+		if(i%2==1)
+			printf("%d",i);
+		i++;
+	}
+	return 0;
+
+
+//	int x;
+//	if(x%2==1);
+//	printf("x是奇数/n");
+
+
+//	if(int x,x%2=1);
+	//	printf("x/n");
+
+
+
+//	return 0;
+//}
